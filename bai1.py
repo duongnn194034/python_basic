6b9086a0a13edf50ef154ffb89d8a1ff5f2738a0
duongnn194034 <67011150+duongnn194034@users.noreply.github.com>
2022-12-18 18:05:59 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -3,8 +3,8 @@ uoc = 0
 for i in range(1, N+1):
     if N % i == 0:
         uoc = uoc + 1
-print('N có ', uoc, ' ước')
-print('Các ước là: ')
+print('N có', uoc, 'ước')
+print('Các ước là:')
 for i in range(1, N+1):
     if N % i == 0:
         print(i)
